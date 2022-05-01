6e2cff1fa272ff0a646750b0bf763b99cca5719d
xiaoyou.qu <xiaoyouPrince@163.com>
2022-05-01 10:51:54 +0800

    json2Model 1.0 完成版本

---
+++ XYDevTool/XYDevTool/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  XYDevTool
+//
+//  Created by 渠晓友 on 2022/4/30.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
