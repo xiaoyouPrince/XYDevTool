9d09566521cd01bc4f030eeb6350465242029f40
xiaoyou.qu <xiaoyouPrince@163.com>
2022-12-03 11:20:38 +0800

    导入ACEView

--- XYDevTool/XYDevTool/XYDevTool-Bridging-Header.h
+++ XYDevTool/XYDevTool/XYDevTool-Bridging-Header.h
@@ -8,6 +8,13 @@
 #ifndef XYDevTool_BridgingHeader_h
 #define XYDevTool_BridgingHeader_h
 
+// STPrivilegedTask
 #import "STPrivilegedTask.h"
+
+// AceView
+#import "ACEView.h"
+#import "ACEModeNames.h"
+#import "ACEThemeNames.h"
+#import "ACEKeyboardHandlerNames.h"
 
 #endif /* XYDevTool_BridgingHeader_h */
