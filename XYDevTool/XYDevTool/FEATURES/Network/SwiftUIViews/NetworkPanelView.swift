d195d8695ebd2cbbe5ae12fca84f46a749965cf2
xiaoyou.qu <xiaoyouPrince@163.com>
2024-08-10 00:31:47 +0800

    请求历史和新请求之间增加拖拽修改尺寸功能

--- XYDevTool/XYDevTool/FEATURES/Network/SwiftUIViews/NetworkPanelView.swift
+++ XYDevTool/XYDevTool/FEATURES/Network/SwiftUIViews/NetworkPanelView.swift
@@ -9,24 +9,64 @@
 import SwiftUI
 
 struct NetworkPanelView: View {
+    @State private var leftWidth: CGFloat = 250 // 初始左侧视图宽度
+    @State private var dividerWidth: CGFloat = 10 // 分隔线宽度
+    
+    
     var body: some View {
-        VStack {
-            PanelTopView()
-            HStack {
-                PanelHistoryView()
-                VStack {
-                    PanelRequestView()
-                    PanelResponceView()
-                }.padding(.trailing, 8)
+        GeometryReader { geometry in
+            VStack {
+                PanelTopView()
+                HStack {
+                    PanelHistoryView()
+                        .frame(width: leftWidth)
+                    
+                    // 可拖拽的分隔线
+                    //DividerView(leftWidth: $leftWidth, dividerWidth: $dividerWidth)
+                    DividerView(leftWidth: self.$leftWidth, dividerWidth: self.$dividerWidth, totalWidth: geometry.size.width)
+                    
+                    VStack {
+                        PanelRequestView()
+                        PanelResponceView()
+                    }.padding(.trailing, 8)
+                }
+                PanelStatusView()
             }
-            PanelStatusView()
+            .frame(minWidth: 350, idealWidth: 700, maxWidth: .infinity,
+                   minHeight: 200, idealHeight: 450, maxHeight: .infinity)
         }
-            .frame(minWidth: 350, idealWidth: 700, maxWidth: NSScreen.main?.frame.size.width,
-            minHeight: 200, idealHeight: 450, maxHeight: NSScreen.main?.frame.size.height)
-            
     }
 }
 
 #Preview {
     NetworkPanelView()
 }
+
+struct DividerView: View {
+    @Binding var leftWidth: CGFloat
+    @Binding var dividerWidth: CGFloat
+    var totalWidth: CGFloat
+    @State private var lastLocation: CGPoint = .zero
+    
+    var body: some View {
+        Rectangle()
+            .fill(Color.gray)
+            .frame(width: dividerWidth)
+            .gesture(
+                DragGesture()
+                    .onChanged { value in
+                        let newWidth = self.leftWidth + value.translation.width
+                        if newWidth >= 0 && newWidth <= self.totalWidth - self.dividerWidth {
+                            self.leftWidth = newWidth
+                        }
+                    }
+            )
+            .onAppear {
+                self.lastLocation = CGPoint(x: self.leftWidth + self.dividerWidth / 2, y: 0)
+            }
+            .transaction { transaction in
+                transaction.animation = nil // 禁用动画
+            }
+    }
+}
+
--- XYDevTool/XYDevTool/FEATURES/Network/SwiftUIViews/PanelRequestView.swift
+++ XYDevTool/XYDevTool/FEATURES/Network/SwiftUIViews/PanelRequestView.swift
@@ -12,28 +12,26 @@ struct PanelRequestView: View {
     @EnvironmentObject var dataModel: NetworkDataModel
     
     var body: some View {
-        ZStack {
-            HStack(spacing: 8) {
-                VStack {
-                    HStack {
-                        Text("请求头(仅支持JSON)")
-                        Spacer()
-                    }
-                    CustomTextEditor(text: $dataModel.httpHeaders)
-                        .frame(maxWidth: .infinity, maxHeight: .infinity)
-                        .border(.background, width: 1)
-                    
+        HStack(spacing: 8) {
+            VStack {
+                HStack {
+                    Text("请求头(仅支持JSON)")
+                    Spacer()
                 }
+                CustomTextEditor(text: $dataModel.httpHeaders)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .border(.background, width: 1)
                 
-                VStack {
-                    HStack {
-                        Text("请求参数(仅支持JSON)")
-                        Spacer()
-                    }
-                    CustomTextEditor(text: $dataModel.httpParameters)
-                        .frame(maxWidth: .infinity, maxHeight: .infinity)
-                        .border(.background, width: 1)
+            }
+            
+            VStack {
+                HStack {
+                    Text("请求参数(仅支持JSON)")
+                    Spacer()
                 }
+                CustomTextEditor(text: $dataModel.httpParameters)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .border(.background, width: 1)
             }
         }
     }
