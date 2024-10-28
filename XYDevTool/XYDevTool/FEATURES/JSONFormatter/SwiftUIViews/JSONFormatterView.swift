ef5ff6952cb03051ce8f35f90367c396832a0570
xiaoyou.qu <xiaoyouPrince@163.com>
2024-10-28 17:25:19 +0800

    Update JSONFormatterView.swift

--- XYDevTool/XYDevTool/FEATURES/JSONFormatter/SwiftUIViews/JSONFormatterView.swift
+++ XYDevTool/XYDevTool/FEATURES/JSONFormatter/SwiftUIViews/JSONFormatterView.swift
@@ -9,7 +9,6 @@
 import SwiftUI
 import Combine
 
-
 struct JSONFormatterView: View {
     @State var text: String = ""
     @State var document = CodeEditorDemoDocument()
@@ -18,8 +17,6 @@ struct JSONFormatterView: View {
         ZStack {
             Color(nsColor: .windowBackgroundColor)
             HStack {
-                //lineNumberView(text: $text)
-                
                 CustomTextEditor(text: $text)
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
                     .border(.background, width: 1)
@@ -27,42 +24,9 @@ struct JSONFormatterView: View {
                     .padding(.leading)
                     .colorScheme(.light)
                 
-//                ContentView(document: $document)
-                
-                
-                
                 JSONFormatterFunctionsView(text: $text)
                     .padding(.trailing)
             }
         }
-    }
-}
-
-//#Preview {
-//    JSONFormatterView()
-//}
-
-
-struct lineNumberView: View {
-    @Binding var text: String
-//    @State var lineNumber: Int = 0
-    static var tv: NSTextView?
-    
-    var body: some View {
-        let lineNumber = getLineNumbers()
-        VStack(alignment: .leading) {
-            ForEach(lineNumber.indices, id: \.self) { idx in
-                Text(lineNumber[idx])
-                    .id(idx)
-            }
-            Spacer()
-        }
-        .padding(.top)
-    }
-    
-    func getLineNumbers() -> [String] {
-        let lineCount = text.components(separatedBy: "\n").count
-        let lineNumbers = (1...lineCount).map { "\($0)" }
-        return lineNumbers
     }
 }
